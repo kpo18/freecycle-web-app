2293030cd11703535b965e1e15cce0c1ed752fff
Katja Pollmann <katja.pollmann@zavamed.com>
2023-02-10 18:26:34 +0000

    Added dropdown hamburger menu

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Header = () => {
+const [isOpen, setIsOpen] = useState(false);
+
+const handleToggle = () => {
+  setIsOpen(!isOpen);
+}
+
   return (
     <header className="header">
       <div className="container header-container">
         <Link to="/">
           <img className="logo" src="/assets/logo.svg" alt="SecondLife Logo" />
         </Link>
-        <div className="flex-end">
+        <div className="flex-end mobile-hidden">
           <Link to="/admin" className="menu-item">
             Admin
           </Link>
@@ -16,6 +22,26 @@ const Header = () => {
             Add item
           </Link>
         </div>
+        {/* Hamburger menu */}
+        <div className="hamburger-menu">
+          <button onClick={handleToggle}
+          className={`hamburger ${isOpen ? "open" : null}`}>
+            <span className="hamburger-top"></span>
+            <span className="hamburger-middle"></span>
+            <span className="hamburger-bottom"></span>
+          </button>
+        </div>
+      </div>
+      {/* Mobile menu */}
+      <div className="desktop-hidden">
+      <div className={`dropdown-mobile ${isOpen ? "flex" : null}`}> 
+      <Link to="/new" className="btn w-100">
+            Add item
+      </Link>
+      <Link to="/admin" className="menu-item w-100">
+            Admin
+      </Link>
+      </div>
       </div>
     </header>
   );
